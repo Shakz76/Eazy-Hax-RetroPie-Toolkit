183c3b29c96a80dadc4ce7f81d4fd49c943eac98
Forrest <forrest.page@gmail.com>
2017-05-26 08:43:07 -0500

    backend config bug fixes.

--- EAZYHAX/aimtrak_setup.sh
+++ EAZYHAX/aimtrak_setup.sh
@@ -8,14 +8,16 @@ if [ ! -d "/home/pi/RetroPie/roms/mame-advmame" ]; then
         sudo /home/pi/RetroPie-Setup/retropie_packages.sh 101
 fi
 
-
+echo ""
+echo ""
 echo "Instlling the needed packages for the gun configureation application."
 sleep 3
 sudo apt-get -y install apt-transport-https python-dev python-pygame python-setuptools
-
+echo ""
+echo ""
 echo "Making advance mame config changes so your Aimtrak will be detected in game."
 sleep 3
-sed -i 's/device_mouse auto/device_mouse auto/g' /home/pi/.advance/advmame-1.4.rc
+sed -i 's/device_mouse auto/device_mouse raw/g' /home/pi/.advance/advmame-1.4.rc
 sed -i 's/device_raw_mousedev\[0\] auto/device_raw_mousedev[0] \/dev\/input\/mouse0/g' /home/pi/.advance/advmame-1.4.rc
 sed -i 's/device_raw_mousedev\[1\] auto/device_raw_mousedev[1] \/dev\/input\/mouse1/g' /home/pi/.advance/advmame-1.4.rc
 sed -i 's/device_raw_mousedev\[2\] auto/device_raw_mousedev[2] \/dev\/input\/mouse2/g' /home/pi/.advance/advmame-1.4.rc
@@ -24,12 +26,21 @@ sed -i 's/device_raw_mousetype\[0\] pnp/device_raw_mousetype[0] ps2/g' /home/pi/
 sed -i 's/device_raw_mousetype\[1\] pnp/device_raw_mousetype[1] ps2/g' /home/pi/.advance/advmame-1.4.rc
 sed -i 's/device_raw_mousetype\[2\] pnp/device_raw_mousetype[2] ps2/g' /home/pi/.advance/advmame-1.4.rc
 sed -i 's/device_raw_mousetype\[3\] pnp/device_raw_mousetype[3] ps2/g' /home/pi/.advance/advmame-1.4.rc
-
-echo "Downloading and installing cheats. I will then enable cheats for Advance-Mame"
+echo ""
+echo ""
+echo "Downloading and installing cheats. I will then enable cheats for Advance-Mame if they are needed."
+sleep 5
+if [ ! -f "/home/pi/.advance/cheat.dat" ]; then
+	echo ""
+    echo ""
+	echo "Cheats are not installed. Installing them and enabling them for you."
+	sleep 5
 cd /home/pi/.advance
 wget https://raw.githubusercontent.com/libretro/mame2003-libretro/master/metadata/cheat.dat
 sed -i 's/misc_cheat no/misc_cheat yes/g' /home/pi/.advance/advmame-1.4.rc
-
+fi
+echo ""
+echo ""
 echo "Downloading gun configureation application. This will appear in your RetroPie menu after install. You will need to calibrate the gun prior to gameplay."
 sleep 8
 git clone https://github.com/gunpadawan/gunconf.git
@@ -38,15 +49,21 @@ sudo cp utils/aimtrak.rules /etc/udev/rules.d/99-aimtrak.rules
 sudo udevadm control --reload-rules
 sudo python setup.py install
 cp ./utils/gunconf.sh ~/RetroPie/retropiemenu/
-
+echo ""
+echo ""
 echo "Checking to see if you have any shooters. If not snagging a few to get you started"
 cd /home/pi/RetroPie/roms/mame-advmame/
 if [ ! -f "/home/pi/RetroPie/roms/mame-advmame/alien3.zip" ]; then
-	wget http://eazyhax.com/pitime/shooter/alien3.zip
-elif [[ ! -f "/home/pi/RetroPie/roms/mame-advmame/le2.zip" ]]; then
-	wget http://eazyhax.com/pitime/shooter/le2.zip
-elif [[ ! -f "/home/pi/RetroPie/roms/mame-advmame/duckhunt.zip" ]]; then
-	wget http://eazyhax.com/pitime/shooter/duckhunt.zip
+        wget http://eazyhax.com/pitime/shooter/alien3.zip
 fi
-
-echo "All done. Enjoy and remember....One shot one kill!"
+if [[ ! -f "/home/pi/RetroPie/roms/mame-advmame/le2.zip" ]]; then
+        wget http://eazyhax.com/pitime/shooter/le2.zip
+fi
+if [[ ! -f "/home/pi/RetroPie/roms/mame-advmame/duckhunt.zip" ]]; then
+        wget http://eazyhax.com/pitime/shooter/duckhunt.zip
+fi
+echo ""
+echo ""
+echo "All done. Enjoy and remember....One shot one kill! Your Pi will now reboot"
+sleep 10
+sudo reboot
