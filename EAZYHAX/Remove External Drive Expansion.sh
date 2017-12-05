0d04b0f95f6fdd1d0f6c12445ecea4463fbaa657
Forrest <forrest.page@gmail.com>
2017-12-05 12:42:54 -0600

    Update Remove External Drive Expansion.sh
    Updated to support new fstab mount designations made by upstream changes from Raspbian to RetroPie

--- EAZYHAX/Remove External Drive Expansion.sh
+++ EAZYHAX/Remove External Drive Expansion.sh
@@ -8,7 +8,7 @@ if [ ! -d /home/pi/RetroPie/localroms ]; then
 	sleep 5
 else
 
-sudo sed -i '/UUID/d' /etc/fstab
+sudo sed -i '/addon/d' /etc/fstab
 sudo cp /home/pi/RetroPie/retropiemenu/EAZYHAX/10-retropie.org /etc/profile.d/10-retropie.sh
 sudo sed -i 's@/home/pi/addonusb/piroms/roms@/home/pi/RetroPie/roms@g' /etc/samba/smb.conf
 runuser -l pi -c 'unlink /home/pi/RetroPie/roms; sudo umount /home/pi/addonusb; sudo umount overlay; sudo umount /home/pi/RetroPie/roms;rm -r /home/pi/RetroPie/roms; mv /home/pi/RetroPie/localroms /home/pi/RetroPie/roms' > /dev/null 2>&1
