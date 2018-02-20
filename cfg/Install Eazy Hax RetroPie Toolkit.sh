e4295b5cb9cefeebe28fa08b2f2160d1586c57a7
Forrest <forrest.page@gmail.com>
2018-02-20 09:15:50 -0600

    ugh

--- cfg/Install Eazy Hax RetroPie Toolkit.sh
+++ cfg/Install Eazy Hax RetroPie Toolkit.sh
@@ -7,7 +7,7 @@ fi
 
 cd /home/pi/RetroPie/retropiemenu
 svn checkout https://github.com/Shakz76/Eazy-Hax-RetroPie-Toolkit/trunk/Eazy%20Hax%20Toolkit
-
+cd /home/pi/RetroPie/retropiemenu/Eazy\ Hax\ Toolkit/
 REV=`svn info | grep "Revision" | awk '{print $2}'`
 DATE=`svn info | grep "Last Changed Date" |awk '{print $4}'`
 TIME=`svn info | grep "Last Changed Date" |awk '{print $5}'`
@@ -15,5 +15,4 @@ echo "You have installed the latest version $REV of the Eazy Hax RetroPie Toolki
 echo "Last update was on $DATE at $TIME"
 echo "You can now run the toolkit directly from your RetroPie Menu under the Eazy Hax Toolkit directory."
 echo "Rebooting your system shortly so that the changes take effect, and or new scripts show up."
-sleed 10
-sudo reboot
+sleep 10
