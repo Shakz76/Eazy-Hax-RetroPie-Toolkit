9a892dfa3f198308a1ba69be95ac7f6f7e2bad99
Forrest <forrest.page@gmail.com>
2018-02-21 06:21:25 -0600

    Curl corrections, switched to env variables rather than hard coded

--- Eazy Hax Toolkit/Expand_Enable.sh
+++ Eazy Hax Toolkit/Expand_Enable.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Script created by Forrest to expand the roms file system to an external
 # So folks will stop getting ripped off for bigger drives on ebay.
-mkdir /home/pi/addonusb > /dev/null 2>&1
-mkdir /home/pi/.work > /dev/null 2>&1
-mkdir /home/pi/RetroPie/combined_drives > /dev/null 2>&1
+EXTROMDIR=piroms
+
+mkdir $HOME/addonusb > /dev/null 2>&1
+mkdir $HOME/.work > /dev/null 2>&1
+mkdir $HOME/RetroPie/combined_drives > /dev/null 2>&1
 
 testdrive=`df |grep media |awk '{print $1 }'|wc -l`
 if [ $testdrive -eq 0 ] ; then
@@ -11,7 +13,7 @@ echo "No External drive detected. Exiting."
 sleep 5
 exit
 else
-echo "External Drive detected. Performing checks for NTFS filesystem and piroms directory."
+echo "External Drive detected. Performing checks for NTFS filesystem and $EXTROMDIR directory."
 sleep 5
 fi
 
@@ -22,16 +24,16 @@ init_usb_mount=`sudo blkid |grep -v mmc|awk '{print $1 }'| sed -r 's/(:)+$//'`
 init_usb_dir=`df |grep media|grep -v mmc |awk '{print $6 }'`
 
 sudo umount $EXTDR
-sudo mount -t  $init_usb_type $init_usb_mount /home/pi/addonusb
-usb_path=`find "/home/pi/addonusb/" |grep piroms |head -1`
+sudo mount -t  $init_usb_type $init_usb_mount $HOME/addonusb
+usb_path=`find "$HOME/addonusb/" |grep $EXTROMDIR |head -1`
 
 if [ ! -d "$usb_path" ]; then
-echo -e "\n\n\nCannot locate the /piroms directory on this external drive. Checking other disks if this external drive has two mount points like a "Western Digital My Book."\n\n\n"
+echo -e "\n\n\nCannot locate the /$EXTROMDIR directory on this external drive. Checking other disks if this external drive has two mount points like a "Western Digital My Book."\n\n\n"
         sudo umount $init_usb_filesystem
     sleep 8
 else
-        usb_path=`find "/home/pi/addonusb/" |grep piroms |head -1`
-                usb_dir=/home/pi/addonusb
+        usb_path=`find "$HOME/addonusb/" |grep $EXTROMDIR |head -1`
+                usb_dir=$HOME/addonusb
 fi
 done
 
@@ -54,20 +56,20 @@ sleep 10
 exit
 else
 
-echo "UUID=$usb_uuid  /home/pi/addonusb      $usb_filesystem    nofail,user,umask=0000  0       2" > /home/pi/.currentdrive
-sudo sh -c 'cat /home/pi/.currentdrive >> /etc/fstab'
+echo "UUID=$usb_uuid  $HOME/addonusb      $usb_filesystem    nofail,user,umask=0000  0       2" > $HOME/.currentdrive
+sudo sh -c 'cat $HOME/.currentdrive >> /etc/fstab'
 sudo umount $usb_mount
 sudo mount -a
 mkdir $usb_path/roms/
-find "/home/pi/RetroPie/roms" -mindepth 1 -maxdepth 1 -type d -printf "$usb_path/roms/%f\n" | xargs mkdir -p 2>/dev/null || true
+find "$HOME/RetroPie/roms" -mindepth 1 -maxdepth 1 -type d -printf "$usb_path/roms/%f\n" | xargs mkdir -p 2>/dev/null || true
 sleep 1
-mv /home/pi/RetroPie/roms /home/pi/RetroPie/localroms
+mv $HOME/RetroPie/roms $HOME/RetroPie/localroms
 cd /etc/samba/
-sudo curl -o https://raw.githubusercontent.com/Shakz76/Eazy-Hax-RetroPie-Toolkit/master/cfg/smb.conf.exp
+sudo curl -o ./smb.conf.exp https://raw.githubusercontent.com/Shakz76/Eazy-Hax-RetroPie-Toolkit/master/cfg/smb.conf.exp
 sudo cp /etc/samba/smb.conf /etc/samba/smb.conf.bkup
 sudo cp /etc/samba/smb.conf.exp /etc/samba/smb.conf
 cd /etc/profile.d
-sudo curl -o https://raw.githubusercontent.com/Shakz76/Eazy-Hax-RetroPie-Toolkit/master/cfg/10-retropie.sh.exp
+sudo curl -o ./10-retropie.sh.exp https://raw.githubusercontent.com/Shakz76/Eazy-Hax-RetroPie-Toolkit/master/cfg/10-retropie.sh.exp
 sudo mv /etc/profile.d/10-retropie.sh /etc/profile.d/10-retropie.sh.org
 sudo cp /etc/profile.d/10-retropie.sh.exp /etc/profile.d/10-retropie.sh
 sudo cp /etc/samba/smb.conf /etc/samba/smb.conf.bkup
