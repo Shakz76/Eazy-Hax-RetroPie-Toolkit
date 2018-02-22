8af1809be4283967260d14e374ed9e063ff456d1
Forrest <forrest.page@gmail.com>
2018-02-22 09:43:05 -0600

    .

--- Eazy Hax Toolkit/Expand to External Drive Disable.sh
+++ Eazy Hax Toolkit/Expand to External Drive Disable.sh
@@ -9,7 +9,9 @@ if [ ! -d $HOME/RetroPie/localroms ]; then
 else
 
 sudo sed -i '/addon/d' /etc/fstab
+sudo unlink /etc/profile.d/10-retropie.sh
 sudo cp /etc/profile.d/10-retropie.sh.org /etc/profile.d/10-retropie.sh
+sudo unlink /etc/samba/smb.conf
 sudo cp /etc/samba/smb.conf.bkup /etc/samba/smb.conf
 sudo /usr/sbin/service smbd stop
 unlink $HOME/RetroPie/roms; sudo umount $HOME/addonusb; sudo umount overlay; mv $HOME/RetroPie/localroms $HOME/RetroPie/roms; rm -r $HOME/RetroPie/combined_drives; rm -r $HOME/addonusb   > /dev/null 2>&1
