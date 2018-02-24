0dc02e00e06bd66e0be8165dcf91dc86dbc3cbaa
Forrest <forrest.page@gmail.com>
2018-02-24 07:29:34 -0600

    dir change

--- Eazy Hax Toolkit/Disk Space.sh
+++ Eazy Hax Toolkit/Disk Space.sh
@@ -4,7 +4,7 @@ DISKSPACE=`df -kh /home/pi/RetroPie/roms | tail -1 | tr -s ' ' | cut -d' ' -f4`
 echo ""
 echo ""
 echo "You have $DISKSPACE available on your RetroPie SD Card."
-if [ -d "/home/pi/addonusb/piroms/roms" ]; then
+if [ -d "/home/pi/addonusb/roms" ]; then
 EXP_DISKSPACE=`df -kh /home/pi/addonusb| tail -1 | tr -s ' ' | cut -d' ' -f4`
 echo ""
 echo ""
