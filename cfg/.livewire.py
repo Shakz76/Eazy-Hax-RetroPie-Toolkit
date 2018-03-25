eda972c7347e9f762cd5df41d5ccd8f565de40cc
Forrest <forrest.page@gmail.com>
2018-03-25 05:29:11 -0500

    Livewire script changes for clean output

--- cfg/.livewire.py
+++ cfg/.livewire.py
@@ -7,7 +7,7 @@ import random
 #CONFIG SECTION
 startdelay = 0 # Value (in seconds) to delay audio start.  If you have a splash screen with audio and the script is playing music over the top of it, increase this value to delay the script from starting.
 musicdir = '/home/pi/RetroPie/roms/music'
-maxvolume = 0.10
+maxvolume = 0.75
 volumefadespeed = 0.02
 restart = True # If true, this will cause the script to fade the music out and -stop- the song rather than pause it.
 startsong = "" # if this is not blank, this is the EXACT, CaSeSeNsAtIvE filename of the song you always want to play first on boot.
