f4ee369cf0f499e7778ec2263546d7eff788d921
Kashaiahyah85 <kashaiahyah85@icloud.com>
2019-09-23 10:10:52 -0700

    Update syntax

--- Eazy Hax Toolkit/Disk Space.sh
+++ Eazy Hax Toolkit/Disk Space.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-DISKSPACE=`df -kh /home/pi/RetroPie/roms | tail -1 | tr -s ' ' | cut -d' ' -f4`
+DISKSPACE=$(df -kh /home/pi/RetroPie/roms | tail -1 | tr -s ' ' | cut -d' ' -f4)
 echo ""
 echo ""
 echo "You have $DISKSPACE available on your RetroPie SD Card."
 if [ -d "/home/pi/addonusb/roms" ]; then
-EXP_DISKSPACE=`df -kh /home/pi/addonusb| tail -1 | tr -s ' ' | cut -d' ' -f4`
+EXP_DISKSPACE=$(df -kh /home/pi/addonusb| tail -1 | tr -s ' ' | cut -d' ' -f4)
 echo ""
 echo ""
 echo "You have $EXP_DISKSPACE available on your RetroPie Expanded External Drive."
